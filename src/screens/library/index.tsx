3d04836077dc6b12b7f4fbb90bccb5d25aa64df5
benilyxdd <benilyxdd@gmail.com>
2023-06-24 22:42:09 +0800

    Created some temporary UI

--- src/screens/library/index.tsx
+++ src/screens/library/index.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, View } from 'react-native';
+
+import { useAppDispatch, useAppSelector } from '@src/redux/hooks';
+import { googleSignOut } from '@src/redux/slices/google-auth/actions';
+import { fetchFolders, getAccessToken } from '@src/services/google-drive-api';
+import TrackPlayer from 'react-native-track-player';
+
+const track = [
+  {
+    url: 'https://free-mp3-download.net/tmp/c8b8e624ed2523157365cc98a8a91aaf/Anne-Marie%20-%202002.mp3',
+    title: '2002',
+    artist: 'anna',
+  },
+];
 
 const Library = () => {
+  const dispatch = useAppDispatch();
+  const { isSignIn } = useAppSelector(state => state.googleAuth);
+  const { isSetUp } = useAppSelector(state => state.trackPlayer);
+  const getDebugInfo = async () => {
+    console.log('is google signed in: ', isSignIn);
+    console.log('google access token: ', getAccessToken());
+    console.log('is track player setup: ', isSetUp);
+  };
+  const signout = async () => {
+    dispatch(googleSignOut());
+  };
+  const add = async () => {
+    await TrackPlayer.add(track);
+  };
+  const test = async () => {
+    fetchFolders();
+    // console.log(await TrackPlayer.getQueue());
+    // console.log(await TrackPlayer.getCurrentTrack());
+    // console.log(await TrackPlayer.getState());
+  };
+  const play = async () => {
+    TrackPlayer.play();
+  };
+  const skip = async () => {
+    TrackPlayer.skipToNext();
+  };
+
   return (
     <View>
-      <Text>Home page</Text>
+      <Button onPress={getDebugInfo} title="Debug" />
+      <Button onPress={signout} title="signout" />
+      <Button onPress={add} title="add" />
+      <Button onPress={test} title="test" />
+      <Button onPress={play} title="play" />
+      <Button onPress={skip} title="skip" />
     </View>
   );
 };
--- src/screens/setting/index.tsx
+++ src/screens/setting/index.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Appbar, List } from 'react-native-paper';
+
+import { useAppDispatch, useAppSelector } from '@src/redux/hooks';
+import { googleSignIn } from '@src/redux/slices/google-auth/actions';
+
+const NotSignInListItems = () => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <>
+      <List.Item
+        // eslint-disable-next-line react/no-unstable-nested-components
+        left={props => <List.Icon {...props} icon="open-in-new" />}
+        title="Click Here to Sign In"
+        onPress={() => dispatch(googleSignIn())}
+      />
+    </>
+  );
+};
+
+const SignedInListItems = () => {
+  return (
+    <>
+      <List.Item title="signed in" />
+    </>
+  );
+};
 
 const Setting = () => {
+  const { isSignIn } = useAppSelector(state => state.googleAuth);
+
   return (
     <View>
-      <Text>setting page</Text>
+      <Appbar.Header elevated={true}>
+        <Appbar.Content title="Settings" />
+      </Appbar.Header>
+
+      <List.Accordion
+        title={`Google Drive ${isSignIn ? '(Connected)' : '(Not Connected)'}`}
+        // eslint-disable-next-line react/no-unstable-nested-components
+        left={props => <List.Icon {...props} icon="google-drive" />}>
+        {!isSignIn && <NotSignInListItems />}
+        {isSignIn && <SignedInListItems />}
+      </List.Accordion>
     </View>
   );
 };
