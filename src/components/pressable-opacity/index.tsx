9029ce015ec8362fa3726bb97aaed3bcda3e6a88
benilyxdd <benilyxdd@gmail.com>
2023-07-07 19:27:16 +0800

    Component add: pressable opacity

---
+++ src/components/pressable-opacity/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Animated, GestureResponderEvent, Pressable } from 'react-native';
+import { Style } from 'twrnc/dist/esm/types';
+
+import tw from '@src/config/twrnc';
+import useAnimation from '@src/hooks/useAnimation';
+
+interface PressableOpacityProps {
+  onPress?: null | ((event: GestureResponderEvent) => void) | undefined;
+  children?: React.ReactNode;
+
+  style?: Style; // twrnc
+}
+
+const PressableOpacity: React.FC<PressableOpacityProps> = ({
+  children,
+  onPress,
+  style,
+}) => {
+  const { fadeIn, fadeOut, opacityValue } = useAnimation();
+
+  return (
+    <Pressable
+      onPressIn={fadeIn}
+      onPressOut={fadeOut}
+      style={[tw`relative`, { ...style }]}
+      onPress={onPress}>
+      {children}
+      <Animated.View
+        style={[
+          tw`absolute top-0 bottom-0 left-0 right-0 bg-gray-100`,
+          { opacity: opacityValue },
+        ]}
+      />
+    </Pressable>
+  );
+};
+
+export default PressableOpacity;
