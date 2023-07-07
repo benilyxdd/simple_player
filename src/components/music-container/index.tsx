73be3f25c7af9f487cf401dabd52b11a29853528
benilyxdd <benilyxdd@gmail.com>
2023-07-07 19:36:48 +0800

    Add flatlist to library screen

--- src/components/music-container/index.tsx
+++ src/components/music-container/index.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
+
+import PressableOpacity from '@src/components/pressable-opacity';
+import tw from '@src/config/twrnc';
+import { Music } from '@src/types/music';
 
 interface MusicContainerProps {
-  name: string;
-  author: string;
+  music: Music;
 }
 
-const MusicContainer: React.FC<MusicContainerProps> = ({ author, name }) => {
+const MusicContainer: React.FC<MusicContainerProps> = ({ music }) => {
+  const { author, id, name } = music;
+  const containerOnPress = () => {};
+
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{author}</Text>
-    </View>
+    <PressableOpacity
+      style={tw`flex flex-row border-b border-gray-500`}
+      onPress={containerOnPress}>
+      <View style={tw`flex flex-col w-4/5`}>
+        <Text
+          style={tw`text-base font-medium`}
+          numberOfLines={1}
+          ellipsizeMode="tail">
+          {name}
+        </Text>
+        <Text style={tw`text-md`}>{author}</Text>
+      </View>
+      <View style={tw`flex justify-center items-center w-1/5`}>
+        <Text>download</Text>
+      </View>
+    </PressableOpacity>
   );
+};
+
+MusicContainer.defaultProps = {
+  music: {
+    id: '-1',
+    author: '',
+    name: '',
+  },
 };
 
 export default MusicContainer;
--- src/screens/library/index.tsx
+++ src/screens/library/index.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { FlatList, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
 
 import MusicContainer from '@src/components/music-container';
 import { useAppSelector } from '@src/redux/hooks';
-import { Appbar } from 'react-native-paper';
+import tw from '@src/config/twrnc';
 
 const Library = () => {
   const { musicFiles } = useAppSelector(state => state.googleDrive);
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={tw`flex-1`}>
       <Appbar.Header elevated={true}>
         <Appbar.Content title="Library" />
       </Appbar.Header>
 
       <FlatList
         data={musicFiles}
-        renderItem={list => {
-          const { name, id, author } = list.item;
-          return <MusicContainer name={name} author={author} />;
-        }}
+        renderItem={list => <MusicContainer music={list.item} />}
       />
     </View>
   );
