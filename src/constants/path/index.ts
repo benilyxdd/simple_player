735a50f690ff7df1feb90aa72f422549aaf86b0a
benilyxdd <benilyxdd@gmail.com>
2023-07-08 21:07:43 +0800

    Add track player uri constant

--- src/constants/path/index.ts
+++ src/constants/path/index.ts
@@ -1,3 +1,5 @@
 import { DocumentDirectoryPath } from 'react-native-fs';
 
 export const MUSIC_FOLDER = `${DocumentDirectoryPath}/music/`;
+export const TRACK_PLAYER_URI = (id: string) =>
+  `file://${MUSIC_FOLDER}/${id}.mp3`;
