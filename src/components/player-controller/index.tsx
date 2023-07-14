3e58e10521fbbed60b4660631ed20b4790a390a1
benilyxdd <benilyxdd@gmail.com>
2023-07-14 17:36:01 +0800

    Updated player controller

--- src/components/player-controller/index.tsx
+++ src/components/player-controller/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
-import { IconButton } from 'react-native-paper';
+import { IconButton, Text } from 'react-native-paper';
 
 import tw from '@src/config/twrnc';
 import TrackPlayer, {
+  Event,
   State,
+  Track,
   usePlaybackState,
+  useTrackPlayerEvents,
 } from 'react-native-track-player';
 
 const PlayerController = () => {
@@ -14,16 +17,33 @@ const PlayerController = () => {
   const play = () => TrackPlayer.play();
   const skipNext = () => TrackPlayer.skipToNext();
 
+  const [currentSong, setCurrentSong] = useState<Track>({} as Track);
+
+  useTrackPlayerEvents([Event.PlaybackTrackChanged], async event => {
+    if (event.type === Event.PlaybackTrackChanged) {
+      const queue = await TrackPlayer.getQueue();
+      setCurrentSong(queue[event.nextTrack]);
+    }
+  });
+
   return (
     <View
-      style={tw`flex flex-row w-full h-12 border-t-2 border-t-gray-500 items-center`}>
-      {/* <IconButton icon="skip-previous" /> */}
-      {playerState === State.Playing ? (
-        <IconButton icon="pause" onPress={pause} />
-      ) : (
-        <IconButton icon="play" onPress={play} />
-      )}
-      <IconButton icon="skip-next" onPress={skipNext} />
+      style={tw`flex flex-row w-full h-12 border-t-2 border-t-gray-500 items-center px-5`}>
+      <View style={tw`flex flex-col h-full w-3/4 justify-center`}>
+        <Text style={tw`text-base font-medium`} ellipsizeMode="tail">
+          {currentSong.title}
+        </Text>
+        <Text style={tw`text-sm`}>{currentSong.artist}</Text>
+      </View>
+      <View
+        style={tw`flex flex-row h-full items-center justify-center flex-shrink flex-grow w-0`}>
+        {playerState === State.Playing ? (
+          <IconButton icon="pause" onPress={pause} />
+        ) : (
+          <IconButton icon="play" onPress={play} />
+        )}
+        <IconButton icon="skip-next" onPress={skipNext} />
+      </View>
     </View>
   );
 };
